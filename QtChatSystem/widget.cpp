989af29dacd18d689bc7eb293b1cf9909e241934
emihiroshi <ab0903he@gmail.com>
2014-10-11 21:51:47 +0900

    ラベルサイズ変更

--- QtChatSystem/widget.cpp
+++ QtChatSystem/widget.cpp
@@ -4,7 +4,7 @@ Widget::Widget(QWidget *parent)
     : QWidget(parent)
 {
     m_seguchi = new QLabel("seguchi", this);
-    m_seguchi->setGeometry(0,0,1920,1080);
+    m_seguchi->setGeometry(0,0,200,200);
     m_seguchi->show();
 }
 
